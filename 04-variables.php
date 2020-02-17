9da54c0cbf30030b9d89df4000050fc15c54b23d
mikhawa <michaeljpitz@gmail.com>
2020-02-17 16:27:44 +0100

    noms de variables

--- 04-variables.php
+++ 04-variables.php
@@ -8,3 +8,14 @@
  * - "-" ne peut être utilisé car c'est un signe de soustraction
  */
 echo "<a href='https://www.php.net/manual/fr/language.variables.basics.php' target='_blank'>Règles de nommage des variables</a>";
+
+// valides
+$mavariable = 5;
+$_coucou = "lulu";
+$p325 = 5;
+
+// non valides
+$15vm = 3;
+$ kjghjhg = 3;
+$aaa-jh = 3;
+
