5fae5a1c45b4b66da0621452e47ba99609b45dd7
mikhawa <michaeljpitz@gmail.com>
2020-03-02 14:50:48 +0100

    ternaire

--- 07-if-alternatifs.php
+++ 07-if-alternatifs.php
@@ -13,11 +13,29 @@
 <p>L'écriture : </p>
 <p> On prend un chiffre au hasard entre 1 et 10 - pour le hazard nous utilisons <a href="https://www.php.net/manual/fr/function.rand.php" target="_blank">rand()</a>"lent",
     <a href="https://www.php.net/manual/fr/function.mt-rand.php" target="_blank">mt_rand()</a>"rapide" ou <a
-        href="https://www.php.net/manual/fr/function.random-int.php" target="_blank">random_int</a>"cryptographie"
+        href="https://www.php.net/manual/fr/function.random-int.php" target="_blank">random_int</a>"cryptographie" :<hr>
 <?php
-$hasard = random_int(1,10);
+$hasard = mt_rand(1,10);
 echo $hasard;
+
+// on peut utiliser le if sans {}, généralement sur une ligne ou quelques unes
+if($hasard>=5) echo "<hr>$hasard est plus grand ou égale à 5";
+
+// on peut utiliser des écritures alternatives:
+if($hasard<3):
+    echo "<hr>$hasard est plus petit que 3";
+else:
+    echo "<hr>$hasard est plus grand ou égale à 3";
+endif;
+
+// condition ternaire, utilisée quand on souhaite un if/else simple | (true)? ceci : ; | (false)? : cela;
+echo ($hasard>5)
+    ? "<hr>$hasard est plus grand que 5"
+    : "<hr>$hasard est plus petit ou égal à 5";
+
 ?>
 </p>
+<hr>
+
 </body>
 </html>
