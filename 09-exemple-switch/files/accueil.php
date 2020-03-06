0c0099dfe56ce580b4f6691e11714ebc09a9e663
mikhawa <michaeljpitz@gmail.com>
2020-03-06 15:09:15 +0100

    header

--- 09-exemple-switch/files/accueil.php
+++ 09-exemple-switch/files/accueil.php
@@ -1,16 +1,23 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
     <title>Accueil</title>
 </head>
 <body>
-<h1>Accueil</h1>
-<div id="head"></div>
-<div id="content"></div>
+<div id="head"><?php include "content/header.php"; ?></div>
+<div id="content">
+    <h1>Accueil</h1>
+    <p>Ici notre accueil</p>
+</div>
 <div id="footer"><?php include "content/footer.php"; ?></div>
+
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
