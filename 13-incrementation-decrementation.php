baa38fb3bd33e623d53b4fc5967ad67e2cd1ad1f
mikhawa <michaeljpitz@gmail.com>
2020-03-11 09:59:27 +0100

    13 incrémentation décrémentation

--- 13-incrementation-decrementation.php
+++ 13-incrementation-decrementation.php
@@ -1,16 +1,24 @@
 <?php
+echo '<p>incrémentation de $a (par 1), $a++ => la modification de la variable est effective dès la ligne suivante (;)</p>';
 $a = 0;
-echo $a++;
+echo $a++; // $a vaut toujours 0 avant le ;
 echo $a;
 echo "<hr>";
+
+
+echo '<p>incrémentation de $a (par 1), ++$a => elle est effective avant la ligne (;) | plus rare</p>';
 $a = 0;
 echo ++$a;
 echo $a;
 echo "<hr>";
+
+echo '<p>décrémentation de $a (par 1), $a-- => la modification de la variable est effective dès la ligne suivante (;)</p>';
 $a = 0;
 echo $a--;
 echo $a;
 echo "<hr>";
+
+echo '<p>décrémentation de $a (par 1), --$a => elle est effective avant la ligne (;) | plus rare</p>';
 $a = 0;
 echo --$a;
 echo $a;
