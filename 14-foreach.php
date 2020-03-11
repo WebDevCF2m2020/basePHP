fd069075f79a82f172a6a35ed93b9786a9dec84b
mikhawa <michaeljpitz@gmail.com>
2020-03-11 10:52:23 +0100

    foreach début

--- 14-foreach.php
+++ 14-foreach.php
@@ -22,7 +22,7 @@ $stagiaires = ["Mattia","Karim","Lorenzo","Bryan","Laetitia","Rocio","Audrey","M
 foreach ($stagiaires as $valeur){
     echo "$valeur | ";
 }
-echo "<hr>"
+echo "<hr>";
 ?>
 <h3>La clef PUIS la valeur avec "as $key => $value"</h3>
 <pre>foreach ($stagiaires as $clef => $valeur){
@@ -33,7 +33,29 @@ echo "<hr>"
 foreach ($stagiaires as $clef => $valeur){
     echo "$clef => $valeur | ";
 }
-echo "<hr>"
+echo "<hr>";
+?>
+<h4>Sur un tableau réellement indexé, on peut aussi utiliser les boucles for, while et même do while</h4>
+<h5>Exemple avec for</h5>
+<?php
+// count compte le nombre d'élément d'un tableau, si le tableau ne change pas de nombre de valeurs pendant la structure itérative, il vaut mieux éviter de le mettre dans la condition du for pour éviter qu'on recompte le nombre d'éléments dans le tableau à chaque itération. !!! count compte à partir de 1!
+$nb_stagiaires = count($stagiaires); // 18
+echo '$nb_stagiaires, équivalent à count($stagiaires) vaut : '.$nb_stagiaires;
+echo "<hr>";
+// de 0 à 17 (les clefs du tableau)
+for($i=0;$i<$nb_stagiaires;$i++){
+    echo "$i => $stagiaires[$i] | ";
+}
+?>
+<p>Vous vous dites "inutile" d'utiliser le for alors qu'on a foreach, le for peut être plus rapide sur un tableau indexé car, si on a pas besoin de toutes les valeurs, le nombre d'itération sera réduite</p>
+<p>Exemple: afficher les 3 premiers</p>
+<pre>for($i=0;$i<3;$i++){
+    echo "$i => $stagiaires[$i] | ";
+}</pre>
+<?php
+for($i=0;$i<3;$i++){
+    echo "$i => $stagiaires[$i] | ";
+}
 ?>
 </body>
 </html>
