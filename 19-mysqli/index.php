3bdd5f1f43240250d9ccc9ae2572f0fb77871acb
mikhawa <michaeljpitz@gmail.com>
2020-03-19 11:23:40 +0100

    request

--- 19-mysqli/index.php
+++ 19-mysqli/index.php
@@ -19,5 +19,13 @@ $db = @mysqli_connect(DB_HOST,DB_LOGIN, DB_PWD, DB_NAME,DB_PORT);
  * Gestion d'erreur en mysqli procédural
  */
 if(!$db){
-    echo "Erreur n° ".mysqli_connect_errno()." Description : ".mysqli_connect_error();
+    // affichage de l'erreur et arrêt du scritp : die()
+    die("Erreur n° ".mysqli_connect_errno()." Description : ".mysqli_connect_error());
 }
+
+// test de requête: SELECT
+$sql = "SELECT * FROM livredor";
+// exécution de la requête
+$request = mysqli_query($db,$sql)
+    // ou en cas d'erreur
+    or die("Erreur n° ".mysqli_errno($db)." Description : ".mysqli_error($db));
