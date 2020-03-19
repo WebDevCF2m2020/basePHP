29f33c65ebd7bc848ab9f7cd7ca44c9b278b3bd9
mikhawa <michaeljpitz@gmail.com>
2020-03-19 10:37:40 +0100

    connection ok

--- 19-mysqli/index.php
+++ 19-mysqli/index.php
@@ -9,3 +9,15 @@
  * Chargement des dépendances
  */
 require_once "config.php";
+
+/*
+ * Connexion procédurale à notre DB
+ */
+$db = @mysqli_connect(DB_HOST,DB_LOGIN, DB_PWD, DB_NAME,DB_PORT);
+
+/*
+ * Gestion d'erreur en mysqli procédural
+ */
+if(!$db){
+    echo "Erreur n° ".mysqli_connect_errno()." Description : ".mysqli_connect_error();
+}
