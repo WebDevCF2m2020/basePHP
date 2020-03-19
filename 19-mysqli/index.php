572c9ff3ac146eecc9dcaa15a7efd75df530e0c7
mikhawa <michaeljpitz@gmail.com>
2020-03-19 11:40:18 +0100

    affichage du commentaire dans index.php

--- 19-mysqli/index.php
+++ 19-mysqli/index.php
@@ -23,9 +23,25 @@ if(!$db){
     die("Erreur n° ".mysqli_connect_errno()." Description : ".mysqli_connect_error());
 }
 
+
+
+
+
+
+
 // test de requête: SELECT
 $sql = "SELECT * FROM livredor";
+
 // exécution de la requête
 $request = mysqli_query($db,$sql)
     // ou en cas d'erreur
     or die("Erreur n° ".mysqli_errno($db)." Description : ".mysqli_error($db));
+
+// tant que l'on a des articles venant du SELECT, on stocke les valeurs dans la variable $item, ligne par ligne
+while($item = mysqli_fetch_assoc($request)){
+    ?>
+<h3><?=$item['titre']?></h3>
+    <p><?=$item['texte']?></p>
+    <p>Par <?=$item['auteur']?> le <?=$item['ladate']?></p>
+<?php
+}
