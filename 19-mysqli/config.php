498fb75e47d5f72de5f8bf137f9972ac5c29380d
mikhawa <michaeljpitz@gmail.com>
2020-03-20 09:25:40 +0100

    basepourtous db externe

--- 19-mysqli/config.php
+++ 19-mysqli/config.php
@@ -7,3 +7,17 @@ define("DB_PWD","");
 define("DB_PORT",3308);
 define("DB_CHARSET","utf8");
 
+/*
+ * pour ceux qui n'ont pas de DB
+ *
+ * pour accéder à phpmyadmin:
+ * https://sql.webmo.fr/index.php?username=ammalberto&server=sql8614.phpnet.org
+ *
+define("DB_HOST","sql8614.webmo.fr");
+define("DB_NAME","basephppourtous");
+define("DB_LOGIN","basephppourtous");
+define("DB_PWD","SN3wh2IfuMJE");
+define("DB_PORT",48614);
+define("DB_CHARSET","utf8");
+ *
+ */
