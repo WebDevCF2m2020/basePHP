f61d57b9e552b3df7f94f0d8ce2de6ce3c2e63ff
mikhawa <michaeljpitz@gmail.com>
2020-03-23 21:14:21 +0100

    prepa c

---
+++ 18-mysqli/correction-function-DateFR/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Correction Exercice DateFR</title>
+</head>
+<body>
+<h1>Correction Exercice DateFR</h1>
+<h2>Valeurs pour tous</h2>
+<p>Liste des valeurs en datetime</p>
+<p><?php
+$a = "2020-03-05 07:41:46";
+$b = "2020-02-25 16:34:05";
+$c = "2020-02-23 18:54:52";
+$d = "2020-02-06 07:44:44";
+$e = "2020-01-03 04:24:45";
+echo "$a | $b | $c | $d | $e";
+    ?></p>
+<h2>Adrien</h2>
+</body>
+</html>
