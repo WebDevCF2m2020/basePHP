9ad1236c5e2ccce65fe56f0ab4f823b89d64f99c
mikhawa <michaeljpitz@gmail.com>
2020-04-09 10:17:58 +0200

    petite jointure exe

---
+++ 24-crud-base/datas/requetes.sql
@@ -0,0 +1,7 @@
+# Toutes les données de utilisateur
+SELECT  * FROM utilisateur;
+
+# toutes les données de thepage
+SELECT * FROM thepage;
+
+# Sélectionnez tous les champs de la table (*) thepage en joignant les utilisateurs (idutilisateur, thelogin,thename) qui ont écrit ces pages
