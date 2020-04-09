c17d88daf133923fbc4d26683be6fe5cd8346fd5
mikhawa <michaeljpitz@gmail.com>
2020-04-09 10:45:43 +0200

    INNER JOIN

--- 24-crud-base/datas/requetes.sql
+++ 24-crud-base/datas/requetes.sql
@@ -5,3 +5,9 @@ SELECT  * FROM utilisateur;
 SELECT * FROM thepage;
 
 # Sélectionnez tous les champs de la table (*) thepage en joignant les utilisateurs (idutilisateur, thelogin,thename) qui ont écrit ces pages
+SELECT thepage.*, 
+				utilisateur.idutilisateur, utilisateur.thelogin, utilisateur.thename
+FROM thepage
+	INNER JOIN utilisateur
+    ON thepage.utilisateur_idutilisateur = utilisateur.idutilisateur
+;
