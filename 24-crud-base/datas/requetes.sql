b0568d0d060620a44e223d40f3682bb4991a53e8
mikhawa <michaeljpitz@gmail.com>
2020-04-09 10:52:47 +0200

    alias internes

--- 24-crud-base/datas/requetes.sql
+++ 24-crud-base/datas/requetes.sql
@@ -11,3 +11,11 @@ FROM thepage
 	INNER JOIN utilisateur
     ON thepage.utilisateur_idutilisateur = utilisateur.idutilisateur
 ;
+
+# Sélectionnez tous les champs de la table (*) thepage en joignant les utilisateurs (idutilisateur, thelogin,thename) qui ont écrit ces pages IDEM + alias internes (le nom des tables thepage => t et utilisateur =>u) ! je n'utilise pas la AS même si il fonctionne, pour ne pas le confondre avec les alias externes (noms des champs en sortie)
+SELECT t.*, 
+				u.idutilisateur, u.thelogin, u.thename
+FROM thepage t
+	INNER JOIN utilisateur u
+    ON t.utilisateur_idutilisateur = u.idutilisateur
+;
