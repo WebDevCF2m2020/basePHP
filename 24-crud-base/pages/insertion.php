5b31e9b12fad886bc31accc8372018bdf05d03b4
mikhawa <michaeljpitz@gmail.com>
2020-04-29 12:29:53 +0200

    insertion ok

--- 24-crud-base/pages/insertion.php
+++ 24-crud-base/pages/insertion.php
@@ -1,13 +1,56 @@
 <?php
 /*
- * Récupération de tous les auteurs pour le choix d'un auteur pour l'article
+ *
+ * Si on a envoyé le formulaire
+ *
  */
-$sql = "SELECT idutilisateur, thelogin, thename FROM utilisateur ORDER BY thelogin ASC;";
-$requestUtil = mysqli_query($db,$sql) or die(mysqli_errno($db));
+// superglobale POST non vide
+if(!empty($_POST)){
+
+    /*
+     * on transforme nos variables POST en variables locales, on va les traîter pour éviter les attaques
+     */
+
+    // si idutilisateur n'est pas convertible en entier, il vaudra 0, sinon cette chaîne devient un entier
+    $idutilisateur = (int) $_POST['idutilisateur'];
+
+    // protection de base trim (les espaces avant après) puis strip_tags (retire les balises) puis conversion des caractères spéciaux en entités html
+    $thetitle = htmlspecialchars(strip_tags(trim($_POST['thetitle'])),ENT_QUOTES);
+    // protection contre les balises, sauf <p><a><br> et <img>
+    $thetext = htmlspecialchars(strip_tags($_POST['thetext'],'<p><a><br><img>'),ENT_QUOTES);
+
+    // si un des champs est vide
+    if(empty($idutilisateur)||empty($thetitle)||empty($thetext)){
+        // redirection vers le formulaire non envoyé
+        header("Location: ./?insertion");
+        exit();
+    // sinon on va insérer la page
+    }else{
+        $sql = "INSERT INTO thepage (thetitle,thetext,utilisateur_idutilisateur) 
+                VALUES ('$thetitle','$thetext',$idutilisateur)";
+        $envoi = mysqli_query($db,$sql);
+
+        // réussite de l'envoi
+        if($envoi){
+            header("Location: ./");
+            exit();
+        }
+    }
+
+
+// on a pas envoyé le formulaire, on sélectionne les auteurs
+}else {
+
+    /*
+     * Récupération de tous les auteurs pour le choix d'un auteur pour l'article
+     */
+    $sql = "SELECT idutilisateur, thelogin, thename FROM utilisateur ORDER BY thelogin ASC;";
+    $requestUtil = mysqli_query($db, $sql) or die(mysqli_errno($db));
 // si on a pas d'utilisateur, on ne peut pas insérer de page, redirection avec header("Location: ./") vers la page d'accueil, on arrête le script de cette page avec exit() ou die()
-if(empty(mysqli_num_rows($requestUtil))){
-    header("Location: ./");
-    exit();
+    if (empty(mysqli_num_rows($requestUtil))) {
+        header("Location: ./");
+        exit();
+    }
 }
 ?>
 <!doctype html>
