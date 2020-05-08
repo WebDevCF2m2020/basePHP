0717723c7c66c60ab055822fe4a000e165d98b28
mikhawa <michaeljpitz@gmail.com>
2020-05-08 11:27:30 +0200

    formulaire

---
+++ 25-mail.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mon formulaire de contact</title>
+</head>
+<body>
+<h1>Mon formulaire de contact</h1>
+<p>Merci de me laisser un message</p>
+<form action="" method="post" name="contact">
+    <input name="lenom" placeholder="Votre nom" required><br>
+    <input name="lemail" placeholder="Votre mail" required><br>
+    <input name="lesujet" placeholder="Sujet" required><br>
+    <textarea name="lemessage" placeholder="Votre message" required></textarea><br>
+    <input type="submit" value="Envoi du mail">
+
+</form>
+</body>
+</html>
