b937f395a0c15cc2e1d187834fc87a8db7ed0780
mikhawa <michaeljpitz@gmail.com>
2020-05-08 11:49:52 +0200

    filtrage des variables POST

--- 25-mail.php
+++ 25-mail.php
@@ -1,3 +1,19 @@
+<?php
+// vérification de l'envoi du formulaire avec tous les champs nécessaires dans un seul isset
+// équivalennt dez multiples isset()&&isset()&&....
+if(isset($_POST['lenom'],$_POST['lemail'],$_POST['lesujet'],$_POST['lemessage'])){
+    /*
+     * Création de variables locales
+     */
+    $lenom = strip_tags(trim($_POST['lenom']));
+    // filter_var avec FILTER_VALIDATE_EMAIL comme attribut va vérifier
+    // si le mail est formaté correctement (pas de vérification de l'existence du mail)
+    // on récupère le mail, sinon $lemail vaut false
+    $lemail = filter_var(trim($_POST['lenom']),"FILTER_VALIDATE_EMAIL");
+    $lesujet = strip_tags(trim($_POST['lesujet']));
+    $lemessage = strip_tags(trim($_POST['lemessage']));
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
