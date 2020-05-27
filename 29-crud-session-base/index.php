03eb2177caed0ce50b6eae6473255f8fbcf45fa5
mikhawa <michaeljpitz@gmail.com>
2020-05-27 11:02:10 +0200

    accueil ok

--- 29-crud-session-base/index.php
+++ 29-crud-session-base/index.php
@@ -11,6 +11,25 @@ require_once "fonctions.php";
 // Appel de notre fonction qui lance une connexion à MySQL
 $connect = connectMysql();
 
+// Requête nécessaire pour afficher tous les articles sur la page d'accueil classés par ladate descendante
+$sql="SELECT a.idarticle, a.titre, LEFT(a.texte,200) AS texte, a.ladate,
+	   u.idutil, u.login, u.nomprenom		
+	FROM article a
+    INNER JOIN util u 
+		ON a.util_idutil = u.idutil
+    ORDER BY a.ladate DESC;";
+
+// exécution de la requête
+$request = mysqli_query($connect,$sql) or die(mysqli_errno($connect));
+
+// on compte le nombre d'article(s)
+$nb = mysqli_num_rows($request);
+
+// vérification de la non existance d'au moins un article:
+if($nb===0){
+    $affiche = "Pas encore d'article pour le moment";
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,5 +42,27 @@ $connect = connectMysql();
 </head>
 <body>
 <h1>Accueil</h1>
+<?php
+if(isset($affiche)){
+?>
+<h2><?=$affiche?></h2>
+<?php
+    // on a au moins un article
+}else{
+    // on crée une ternaire contanant un "s" si on a plus qu'un article
+    $pluriel = ($nb>1) ? "s" : "";
+
+    echo "<h3>Nous avons $nb résultat$pluriel</h3>";
+    // tant qu'on a des articles comme résultat de notre requête, on les stockent dans un tableau associatif d'une ligne
+    while ($item = mysqli_fetch_assoc($request)){
+        ?>
+<hr>
+<h3><?=$item['titre']?></h3>
+        <p><?=$item['texte']?> ... <a href="detailarticle.php?id=<?=$item['idarticle']?>">Lire la suite</a> </p>
+        <h5>Le <?=$item['ladate']?> par <?=$item['nomprenom']?> surnommé.e <?=$item['login']?></h5>
+        <?php
+    }
+}
+?>
 </body>
 </html>
