25776e3e5404942c2484dd275267fa51eca224a5
Michael J. Pitz <michaeljpitz@gmail.com>
2021-04-11 18:28:25 +0200

    Update config.php

--- 19-mysqli/config.php
+++ 19-mysqli/config.php
@@ -7,19 +7,3 @@ define("DB_PWD","");
 define("DB_PORT",3308);
 define("DB_CHARSET","utf8");
 
-/*
- * pour ceux qui n'ont pas de DB
- *
- * pour accéder à phpmyadmin:
- * https://sql.webmo.fr/index.php?username=ammalberto&server=sql8614.phpnet.org
- *
-define("DB_HOST","sqlgold.webmo.fr");
-define("DB_NAME","basephppourtous");
-define("DB_LOGIN","basephppourtous");
-define("DB_PWD","SN3wh2IfuMJE");
-define("DB_PORT",48614);
-define("DB_CHARSET","utf8");
- *
- * le docs contenant ces paramètres:
-https://docs.google.com/document/d/10vi6TQyyxvN8nZTc_LmcL9ImZUhJIJx7piVVfcRo4yg/edit?usp=sharing
- */
